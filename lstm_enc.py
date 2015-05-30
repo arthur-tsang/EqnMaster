a99b23910ef821983f01818b3eb66d864c6c4ea0
arthur-tsang <arthurltsang@gmail.com>
2015-05-30 14:49:50 -0700

    fixed up theano definitions for fprop (except scan)

--- lstm_enc.py
+++ lstm_enc.py
@@ -1,11 +1,11 @@
 import numpy as np
 import theano.tensor as T
 from theano import function, shared
+from theano.tensor.nnet import sigmoid, softmax
 
 from misc import random_weight_matrix
 
-sigmoid = T.nnet.sigmoid
-rng = numpy.random
+rng = np.random
 
 class LSTMEnc:
 
@@ -21,23 +21,6 @@ class LSTMEnc:
         np.random.seed(rseed)
         sigma = .1
 
-        # self.params = {}
-        # self.params['L'] = np.random.normal(0, sigma, (wdim, vdim)) # "wide" array
-        # # Input gate params
-        # self.params['Wi'] = random_weight_matrix(hdim, wdim)
-        # self.params['Ui'] = random_weight_matrix(hdim, hdim)
-        # # Forget gate params
-        # self.params['Wf'] = random_weight_matrix(hdim, wdim)
-        # self.params['Uf'] = random_weight_matrix(hdim, hdim)
-        # # Output gate params
-        # self.params['Wo'] = random_weight_matrix(hdim, wdim)
-        # self.params['Uo'] = random_weight_matrix(hdim, hdim)
-        # # "New" memory cell params
-        # self.params['Wc'] = random_weight_matrix(hdim, wdim)
-        # self.params['Uc'] = random_weight_matrix(hdim, hdim)
-        
-        # skipping biases (at least for now) for simplicity
-
         # Learning rate
         self.alpha = alpha
 
@@ -45,18 +28,12 @@ class LSTMEnc:
         self.rho = rho
 
 
-        # start doing theano stuff
-        # define complicated function
-        # store that in self.something...
-        # Wi = T.dmatrix('Wi')
-        # Ui = T.dmatrix('Ui')
-        # Wf = T.dmatrix('Wf')
-        # Uf = T.dmatrix('Uf')
-        # Wo = T.dmatrix('Wo')
-        # Uo = T.dmatrix('Uo')
-        # Wc = T.dmatrix('Wc')
-        # Uc = T.dmatrix('Uc')
-        # params as theano.shared matrices
+        ## Theano stuff
+
+        # Params as theano.shared matrices
+        self.L = shared(random_weight_matrix(wdim, vdim), name='L')
+        # W: times character-vector, U: times previous-hidden-vector
+        # i: input, f: forget, o: output, c: new-cell
         self.Wi = shared(random_weight_matrix(hdim, wdim), name='Wi')
         self.Ui = shared(random_weight_matrix(hdim, hdim), name='Ui')
         self.Wf = shared(random_weight_matrix(hdim, wdim), name='Wf')
@@ -65,32 +42,43 @@ class LSTMEnc:
         self.Uo = shared(random_weight_matrix(hdim, hdim), name='Uo')
         self.Wc = shared(random_weight_matrix(hdim, wdim), name='Wc')
         self.Uc = shared(random_weight_matrix(hdim, hdim), name='Uc')
+        # and for computing yhat and costs
+        self.Uy = shared(random_weight_matrix(vdim, hdim), name='Uy')
+        # skipping biases (at least for now) for simplicity
         
-        
-        x_t = T.vector('x_t')
+        # Inputs to calculate next h
+        x_t = T.scalar('x_t', dtype='int32') # TODO: what's the best type of int?
+        y_t = T.scalar('y_t', dtype='int32')
         h_prev = T.vector('h_prev')
+        c_prev = T.vector('c_prev')
+        Lx_t = self.L[:,x_t]
+
 
         # gates (input, forget, output)
-        i_t = sigmoid(T.dot(self.Wi, x_t) + T.dot(self.Ui, h_prev))
-        f_t = sigmoid(T.dot(self.Wf, x_t) + T.dot(self.Uf, h_prev))
-        o_t = sigmoid(T.dot(self.Wo, x_t) + T.dot(self.Uo, h_prev))
+        i_t = sigmoid(T.dot(self.Wi, Lx_t) + T.dot(self.Ui, h_prev))
+        f_t = sigmoid(T.dot(self.Wf, Lx_t) + T.dot(self.Uf, h_prev))
+        o_t = sigmoid(T.dot(self.Wo, Lx_t) + T.dot(self.Uo, h_prev))
         # new memory cell
-        c_new_t = T.tanh(T.dot(self.Wc, x_t) + T.dot(self.Uc, h_prev))
+        c_new_t = T.tanh(T.dot(self.Wc, Lx_t) + T.dot(self.Uc, h_prev))
         # final memory cell
         c_t = f_t * c_prev + i_t * c_new_t
         # final hidden state
         h_t = o_t * T.tanh(c_t)
-        
+        # cost
+        y_hats = softmax(T.dot(self.Uy, h_t))
+        cell_cost = -T.log(y_hats[y_t])
+
         # Compile!
-        self.next_h = function([x_t, h_prev], h_t)
-        
+        self.calc_h = function([x_t, h_prev, c_prev], h_t)
+        self.calc_cost = function([h_t, y_t], cell_cost)
 
         # Anything else? maybe...
 
     def f_prop(self, xs, ys):
         x = xs[0]
         y = ys[0]
-        print self.next_h(x, np.zeros(5))
+        print self.calc_h(x, np.zeros(5), np.zeros(5))
+        # TODO: figure out how to use scan to do f_prop efficiently!
 
 
 if __name__ == '__main__':
