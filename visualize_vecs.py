9c1bb5e3a20a7e8918758be2b08768a1baf1ed41
arthur-tsang <arthurltsang@gmail.com>
2015-05-30 19:24:55 -0700

    working on visualize vecs

--- visualize_vecs.py
+++ visualize_vecs.py
@@ -1,5 +1,25 @@
+#!/usr/bin/env python
 import numpy as np
+import matplotlib.pyplot as plt
 
-def vis_word_vecs(L):
-	U, S, V = np.linalg(L)
-	
+def svd_visualize(L):
+        """Visualize L, which we assume is given as an np array of rows"""
+	U, S, V = np.linalg.svd(L)
+	#datapoints = U[:,:2]
+        
+        data_x = U[:,0]
+        data_y = U[:,1]
+
+        fig = plt.figure()
+        fig.suptitle('Word vectors SVD')
+        plt.scatter(data_x, data_y)
+        fig.savefig('figs/svd.jpg')
+        plt.show()
+
+        # TODO: annotate points
+        # http://stackoverflow.com/questions/5147112/matplotlib-how-to-put-individual-tags-for-a-scatter-plot
+
+if __name__ == '__main__':
+        L = np.array([[1,2,3],[4,5,6],[7,8,9],[1,1,1]])
+        svd_visualize(L)
+        
