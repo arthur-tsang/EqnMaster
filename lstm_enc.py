fc42a8d2feb352f533d9989b5b141c676bb98c97
arthur-tsang <arthurltsang@gmail.com>
2015-05-31 22:33:22 -0700

    we think we finished the lstm theano encoder

--- lstm_enc.py
+++ lstm_enc.py
@@ -42,37 +42,29 @@ class LSTMEnc:
         self.Uo = shared(random_weight_matrix(hdim, hdim), name='Uo')
         self.Wc = shared(random_weight_matrix(hdim, wdim), name='Wc')
         self.Uc = shared(random_weight_matrix(hdim, hdim), name='Uc')
-        # and for computing yhat and costs
-        # self.Uy = shared(random_weight_matrix(vdim, hdim), name='Uy')
-        # skipping biases (at least for now) for simplicity
-        
-        # # Inputs to calculate next h
-        # x_t = T.scalar('x_t', dtype='int32') # TODO: what's the best type of int?
-        # # y_t = T.scalar('y_t', dtype='int32')
-        # ch_prev = T.vector('ch_prev')
-        # c_prev = ch_prev[:self.hdim]#T.vector('c_prev')
-        # h_prev = ch_prev[self.hdim:]#T.vector('h_prev')
 
-        # Lx_t = self.L[:,x_t]
+        self.dL = shared(np.zeros(wdim, vdim), name='dL')
+        # W: times character-vector, U: times previous-hidden-vector
+        # i: input, f: forget, o: output, c: new-cell
+        self.dWi = shared(np.zeros(hdim, wdim), name='dWi')
+        self.dUi = shared(np.zeros(hdim, hdim), name='dUi')
+        self.dWf = shared(np.zeros(hdim, wdim), name='dWf')
+        self.dUf = shared(np.zeros(hdim, hdim), name='dUf')
+        self.dWo = shared(np.zeros(hdim, wdim), name='dWo')
+        self.dUo = shared(np.zeros(hdim, hdim), name='dUo')
+        self.dWc = shared(np.zeros(hdim, wdim), name='dWc')
+        self.dUc = shared(np.zeros(hdim, hdim), name='dUc')
 
+        self.params = [self.L, self.Wi, self.Ui, self.Wf, self.Uf, self.Wo, self.Uo, self.Wc, self.Uc]
+        self.dparams = [self.dL, self.dWi, self.dUi, self.dWf, self.dUf, self.dWo, self.dUo, self.dWc, self.dUc]
 
-        # # gates (input, forget, output)
-        # i_t = sigmoid(T.dot(self.Wi, Lx_t) + T.dot(self.Ui, h_prev))
-        # f_t = sigmoid(T.dot(self.Wf, Lx_t) + T.dot(self.Uf, h_prev))
-        # o_t = sigmoid(T.dot(self.Wo, Lx_t) + T.dot(self.Uo, h_prev))
-        # # new memory cell
-        # c_new_t = T.tanh(T.dot(self.Wc, Lx_t) + T.dot(self.Uc, h_prev))
-        # # final memory cell
-        # c_t = f_t * c_prev + i_t * c_new_t
-        # # final hidden state
-        # h_t = o_t * T.tanh(c_t)
-        # # final joint state
-        # ch_t = T.concatenate([h_t, c_t])
-        # # Compile!
-        # self.calc_ch = function([x_t, ch_prev], ch_t)
+        self.f_prop_function = self.compile_f_prop() 
+        self.b_prop_function = self.compile_b_prop()
 
-        # Anything else? maybe...
-        self.f_prop_function = self.compile_f_prop()
+    def reset_grads(self):
+        """Resets all grads to zero (maintaining shape!)"""
+        for dparam in self.dparams:
+            dparam.set_value(0 * dparam.get_value())
 
     def lstm_timestep(self, x_t, ch_prev):
         """calculates info to pass to next time step.
@@ -115,18 +107,25 @@ class LSTMEnc:
 
         return function([xs, ch_prev], self.symbolic_f_prop(xs, ch_prev))
         
-
-    def compile_b_prop(self):
-        """one-time create b_prop function"""
-        delta = T.vector('delta')
-        # delta represents derivative of cost wrt ch_inter
-        # gradient of ch_inter wrt each param
-        # TODO: figure out tomorrow...
-        pass
-
-
     def f_prop(self, xs):
         return self.f_prop_function(xs,np.zeros(2*self.hdim))
+
+    def symbolic_b_prop(self, cost_final):
+        new_dparams = []
+        for param in self.params:
+            new_dparams.append(T.grad(cost_final, param))
+
+        return new_dparams
+        
+    def compile_b_prop(self):
+        cost_final = T.scalar('cost_final')
+        return function([cost_final], self.symbolic_b_prop(cost_final))
+
+    def b_prop(self, cost_final):
+        new_dparams = self.b_prop_function(cost_final)
+        for dparam, new_dparam in zip(self.dparams, new_dparams):
+            dparam.set_value(new_dparam + dparam.get_value())
+        
 
 
 if __name__ == '__main__':
