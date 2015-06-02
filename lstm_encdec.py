98056002505b5602073ac2799f38c85a1891e3f7
arthur-tsang <arthurltsang@gmail.com>
2015-06-01 18:54:35 -0700

    enc dec compiles!

--- lstm_encdec.py
+++ lstm_encdec.py
@@ -48,52 +48,40 @@ class LSTMEncDec:
     def symbolic_b_prop(self, cost):
         dec_new_dparams = self.decoder.symbolic_b_prop(cost)
         enc_new_dparams = self.encoder.symbolic_b_prop(cost)
-        return [dec_new_dparams, enc_new_dparams]
+
+        return dec_new_dparams + enc_new_dparams # python-list concatenation
 
     def compile_both(self):
         xs = T.ivector('xs')
         ys = T.ivector('ys')
         
         cost = self.symbolic_f_prop(xs, ys)
-        print 'time 0'
         new_dparams = self.symbolic_b_prop(cost)
-        print 'time 1'
 
-        return function([xs,ys], new_dparams) # TODO: remove this line
-        'forward prop compiles'
-
-
-        return function([xs, ys], [cost, new_dparams])
+        return function([xs, ys], [cost] + new_dparams)
         
 
     def update_params(dec_enc_new_dparams):
         dec_new_dparams, enc_new_dparams = dec_enc_new_dparams
-        for param, dparam in zip(self.decoder.params, dec_new_dparams) + zip(self.encoder.params, enc_new_params):
+        for param, dparam in zip(self.decoder.params + self.encoder.params, dec_enc_new_dparams):
             param.set_value(param.get_value() + dparam)
 
     def process_batch(self, all_xs, all_ys):
         
-        # def sum_dparams(dparams1, dparams2):
-        #     dec_dparams1, enc_dparams1 = dparams1
-        #     dec_dparams2, enc_dparams2 = dparams2
-        #     dec_dparams = [a + b for a,b in zip(dec_dparams1, dec_dparams2)]
-        #     enc_dparams = [a + b for a,b in zip(enc_dparams1, enc_dparams2)]
-        #     return (dec_dparams, enc_dparams)
 
-        all_dec_dparams = []
-        all_enc_dparams = []
+        all_dparams = []
         tot_cost = 0.0
         batch_size = len(all_xs)
         for xs, ys in zip(all_xs, all_ys):
-            cost, (dec_dparams, enc_dparams) = self.both_prop_compiled(xs, ys)
+            cost_and_dparams = self.both_prop_compiled(xs, ys)
+            cost = cost_and_dparams[0]
+            dparams = cost_and_dparams[1:]
             
-            all_dec_dparams.append(dec_dparams)
-            all_enc_dparams.append(enc_dparams)
+            all_dparams.append(dparams)
             tot_cost += cost
         
-        dec_dparams = np.average(all_dec_dparams, axis=0)
-        enc_dparams = np.average(all_enc_dparams, axis=0)
-        update_params((dec_dparams, enc_dparams))
+        dparams = np.average(all_dparams, axis=0)
+        update_params(dparams)
 
         # forget about regularizing for now
 
@@ -106,7 +94,7 @@ class LSTMEncDec:
 if __name__ == '__main__':
     lstm = LSTMEncDec(12,12,12,12)
 
-    # print 'processing batch'
-    # cost = lstm.process_batch([[1,2,3],[2,2,2]], [[3,2],[0,0]])
-    # print cost
-    # print 'all done'
+    print 'processing batch'
+    cost = lstm.process_batch([[1,2,3],[2,2,2]], [[3,2],[0,0]])
+    print cost
+    print 'all done'
