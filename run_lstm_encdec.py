66e15249e825202e17d31e618d3bc9307cf254d3
arthur-tsang <arthurltsang@gmail.com>
2015-06-03 02:46:15 -0700

    fixed subtraction data

--- run_lstm_encdec.py
+++ run_lstm_encdec.py
@@ -10,7 +10,7 @@ from run_helpers2 import decode, model_solve, preprocess_data, outvocab, invocab
 if __name__ == '__main__':
 
     ## Filename to save LSTMEncDec
-    model_filename = 'models/led_100.p'
+    model_filename = 'models/lstm_test.p'
 
     ## Data
     X_train, Y_train = preprocess_data('data/subtr_train.p', asNumpy = False)
@@ -22,20 +22,11 @@ if __name__ == '__main__':
     outdim = len(outvocab)
     vdim = len(invocab)
     batch_size = 5
-    n_epochs = 500
+    n_epochs = 20
 
-    # X_train = X_train[:100]
-    # Y_train = Y_train[:100]
-
-    # X_train = X_train[:20]
-    # Y_train = Y_train[:20]
-
-    # X_train = [[4,5,1,10,11,10,8,2,6]]
-    # Y_train = [[1,2,7,7]]
-
-
-    X_train = X_train[:1000]
-    Y_train = Y_train[:1000]
+    # TODO: comment out the next two lines
+    X_train = X_train[:10]
+    Y_train = Y_train[:10]
     
     ## EncDec model train
     led = LSTMEncDec(vdim, hdim, wdim, outdim, alpha=0.01, rho = 0.0000)
