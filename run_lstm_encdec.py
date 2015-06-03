ea1b815984ab7e11791b1e9bb13b78d653b890fa
arthur-tsang <arthurltsang@gmail.com>
2015-06-03 00:58:52 -0700

    commented out visualize vecs lines (for better portability)

--- run_lstm_encdec.py
+++ run_lstm_encdec.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 from lstm_encdec import LSTMEncDec
-from visualize_vecs import svd_visualize, tsne_visualize, pca_visualize
+#from visualize_vecs import svd_visualize, tsne_visualize, pca_visualize
 
 from run_helpers import decode, model_solve, preprocess_data, outvocab, invocab
 
