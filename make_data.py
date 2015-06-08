7d0541502fb5719c260c1d2499bda5d029d88905
arthur-tsang <arthurltsang@gmail.com>
2015-06-08 03:25:52 -0700

    made bigmult data; also cleaned make_data.py

--- make_data.py
+++ make_data.py
@@ -4,7 +4,7 @@ import pickle
 
 import numpy as np
 
-# TODO: this code style is horrible
+# TODO: sort the functions in a way that makes sense
 
 def composition_example():
     def randnr():
@@ -35,34 +35,28 @@ def add_ndigs_example(ndigs):
     return (full_string, str(eval(full_string)))
 
 
-def simple_example():
+def simple_example(operator = ' + '):
+    assert(type(operator) == type(' '))
     first = np.random.randint(1000)
     second = np.random.randint(1000)
-    x = str(first) + ' + ' + str(second)
-    y = str(first + second)
+    x = str(first) + operator + str(second)
+    y = str(eval(x))
     return (x,y)
+
+
 
 def very_simple_example():
     first = np.random.randint(100)
     second = np.random.randint(100)
     x = str(first) + '+' + str(second)
-    y = str(first + second)
+    y = str(eval(x))
     return (x,y)
 
 def mult_example():
-    first = np.random.randint(1000)
-    second = np.random.randint(1000)
-    x = str(first) + '*' + str(second)
-    y = str(first * second)
-    return (x,y)
+    return simple_example('*')
 
 def subtr_example():
-    first = np.random.randint(1000)
-    second = np.random.randint(1000)
-    x = str(first) + '-' + str(second)
-    y = str(first - second)
-    return (x,y)
-
+    return simple_example('-')
 
 def simple_discr_example():
     # Come up with an (x,y) pair that doesn't match
@@ -91,192 +85,28 @@ def generic_discr_example(example_generator):
 
 
 def generate_examples():
-    n_train = 10000
-    n_dev = 2000
-    n_test = 2000
-
-    train_file = 'data/train.txt'
-    dev_file = 'data/dev.txt'
-    test_file = 'data/test.txt'
-
-    train_dat = [simple_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [simple_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [simple_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-    
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated positive examples of sizes', len(train_dat), len(dev_dat), len(test_dat)
-
+    generate_generic_example('3dig', simple_example)
 
 def generate_discr_examples():
-    n_train = 10000
-    n_dev = 2000
-    n_test = 2000
-
-    train_file = 'data/neg_train.txt'
-    dev_file = 'data/neg_dev.txt'
-    test_file = 'data/neg_test.txt'
-
-    train_dat = [simple_discr_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [simple_discr_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [simple_discr_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-    
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated discriminative data of sizes', len(train_dat), len(dev_dat), len(test_dat)
+    generate_generic_examples('d_add', simple_discr_example)
 
 def generate_easy_examples():
     """Two-digit addition, no spaces"""
-    n_train = 500
-    n_dev = 100
-    n_test = 100
-
-    train_file = 'data/2dig_train.p'
-    dev_file = 'data/2dig_dev.p'
-    test_file = 'data/2dig_test.p'
-
-    train_dat = [very_simple_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [very_simple_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [very_simple_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated positive examples of sizes', len(train_dat), len(dev_dat), len(test_dat)
+    generate_generic_examples('2dig', very_simple_example, n_train=500, n_dev=100, n_test=100)
 
 def generate_mult_examples():
     """Three-digit multiplication, no spaces"""
-    n_train = 10000
-    n_dev = 2000
-    n_test = 2000
-
-    train_file = 'data/mult_train.p'
-    dev_file = 'data/mult_dev.p'
-    test_file = 'data/mult_test.p'
-
-    train_dat = [mult_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [mult_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [mult_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated positive examples of sizes', len(train_dat), len(dev_dat), len(test_dat)
-
+    generate_generic_examples('mult', mult_example)
 
 def generate_subtr_examples():
     """Three-digit subtraction, no spaces"""
-    n_train = 10000
-    n_dev = 1000
-    n_test = 1000
-
-    train_file = 'data/subtr_train.p'
-    dev_file = 'data/subtr_dev.p'
-    test_file = 'data/subtr_test.p'
-
-    train_dat = [subtr_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [subtr_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [subtr_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated positive examples of sizes', len(train_dat), len(dev_dat), len(test_dat)    
+    generate_generic_examples('subtr', subtr_example)
 
 def generate_subtr_discr_examples():
-    n_train = 10000
-    n_dev = 1000
-    n_test = 1000
-
-    train_file = 'data/d_subtr_train.p'
-    dev_file = 'data/d_subtr_dev.p'
-    test_file = 'data/d_subtr_test.p'
-
-    train_dat = [subtr_discr_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [subtr_discr_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [subtr_discr_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated discrimintative examples of sizes', len(train_dat), len(dev_dat), len(test_dat)    
-
+    generate_generic_examples('d_subtr', subtr_discr_example)
 
 def generate_mult_discr_examples():
-    n_train = 10000
-    n_dev = 2000
-    n_test = 2000
-
-    train_file = 'data/d_mult_train.p'
-    dev_file = 'data/d_mult_dev.p'
-    test_file = 'data/d_mult_test.p'
-
-    train_dat = [mult_discr_example() for _ in xrange(n_train)]
-
-    dev_dat_raw = [mult_discr_example() for _ in xrange(n_dev)]
-    dev_dat = [x for x in dev_dat_raw if x not in train_dat]
-
-    test_dat_raw = [mult_discr_example() for _ in xrange(n_test)]
-    test_dat = [x for x in test_dat_raw if x not in train_dat and x not in dev_dat]
-    
-    with open(train_file, 'w') as f:
-        pickle.dump(train_dat, f)
-    with open(dev_file, 'w') as f:
-        pickle.dump(dev_dat, f)
-    with open(test_file, 'w') as f:
-        pickle.dump(test_dat, f)
-
-    print 'Generated discriminative data of sizes', len(train_dat), len(dev_dat), len(test_dat)
+    generate_generic_examples('d_mult', mult_discr_example)
 
 def triple_pickle(filenames, datasets):
     # Just a convenience function to help you pickle data
@@ -287,55 +117,11 @@ def triple_pickle(filenames, datasets):
     
 
 def generate_composition_examples():
-    n_train = 500000
-    n_dev = 2000
-    n_test = 2000
-    
-    train_file = 'data/comp_train.p'
-    dev_file = 'data/comp_dev.p'
-    test_file = 'data/comp_test.p'
-
-    n_tot = n_train + n_dev + n_test
-    all_examples = set()
-    while len(all_examples) < n_tot:
-        if len(all_examples) % 10000 == 0:
-            print 'len', len(all_examples)
-        all_examples.add(composition_example())
-
-    example_list = list(all_examples)
-    train_dat = example_list[:n_train]
-    dev_dat = example_list[n_train:n_train + n_dev]
-    test_dat = example_list[n_train + n_dev:]
-
-    #triple_pickle(['data/comp_train_short.p'],[train_dat[:10000]])
-    triple_pickle((train_file, dev_file, test_file), (train_dat, dev_dat, test_dat))
-
+    generate_generic_examples('comp', composition_example, n_train = 500000)
     print 'generated composition examples'
     
 def generate_discr_composition_examples():
-    n_train = 10000
-    n_dev = 2000
-    n_test = 2000
-    
-    train_file = 'data/d_comp_train.p'
-    dev_file = 'data/d_comp_dev.p'
-    test_file = 'data/d_comp_test.p'
-
-    n_tot = n_train + n_dev + n_test
-    all_examples = set()
-    while len(all_examples) < n_tot:
-        if len(all_examples) % 10000 == 0:
-            print 'len', len(all_examples)
-        all_examples.add(composition_example())
-
-    example_list = list(all_examples)
-    train_dat = example_list[:n_train]
-    dev_dat = example_list[n_train:n_train + n_dev]
-    test_dat = example_list[n_train + n_dev:]
-
-    #triple_pickle(['data/comp_train_short.p'],[train_dat[:10000]])
-    triple_pickle((train_file, dev_file, test_file), (train_dat, dev_dat, test_dat))
-
+    generate_generic_examples('d_comp', comp_discr_example)
     print 'generated discriminative composition examples'
     
 def generate_generic_examples(file_short, example_generator, n_train=10000, n_dev=2000, n_test=2000):
@@ -378,4 +164,5 @@ if __name__ == '__main__':
     #print composition_example()
     
     # print add_ndigs_example(5)
-    generate_ndig_examples()
+    #generate_ndig_examples()
+    generate_generic_examples('bigmult', mult_example, n_train=100000)
